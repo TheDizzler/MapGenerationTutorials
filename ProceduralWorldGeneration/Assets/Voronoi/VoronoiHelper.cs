1e8ec81e3944f2ab77e3a9451813447bf9a22720
TheDizzler <tgillon.music@gmail.com>
2020-12-01 11:10:36 +0900

    helper methods more consistent

--- ProceduralWorldGeneration/Assets/Voronoi/VoronoiHelper.cs
+++ ProceduralWorldGeneration/Assets/Voronoi/VoronoiHelper.cs
@@ -6,19 +6,22 @@ namespace AtomosZ.Voronoi.Helpers
 	{
 		public static void Associate(Polygon polygon, Corner corner)
 		{
-			if (!polygon.corners.Contains(corner))
+			if (polygon.corners.Contains(corner))
 			{
-				polygon.corners.Add(corner);
-				if (corner.isOOB)
-				{
-					polygon.oobCorners.Add(corner);
-					polygon.isOnBorder = true;
-				}
-				else if (corner.isOnBorder)
-					polygon.isOnBorder = true;
-			}
-			else
+				VoronoiGenerator.debugCorners.Add(corner);
+				VoronoiGenerator.debugPolygons.Add(polygon);
 				throw new Exception("polygon already contains this corner");
+			}
+
+			polygon.corners.Add(corner);
+			if (corner.isOOB)
+			{
+				polygon.oobCorners.Add(corner);
+				polygon.isOnBorder = true;
+			}
+			else if (corner.isOnBorder)
+				polygon.isOnBorder = true;
+
 			if (!corner.polygons.Contains(polygon))
 				corner.polygons.Add(polygon);
 		}
@@ -26,12 +29,21 @@ namespace AtomosZ.Voronoi.Helpers
 		public static void Associate(Polygon polygon, VEdge edge)
 		{
 			if (polygon.voronoiEdges.Contains(edge))
+			{
+				VoronoiGenerator.debugEdges.Add(edge);
+				VoronoiGenerator.debugPolygons.Add(polygon);
 				throw new Exception("polygon already contains this edge");
-			else
-				polygon.voronoiEdges.Add(edge);
+			}
+
+			polygon.voronoiEdges.Add(edge);
 
 			if (edge.Contains(polygon))
+			{
+				VoronoiGenerator.debugEdges.Add(edge);
+				VoronoiGenerator.debugPolygons.Add(polygon);
 				throw new Exception("edge already contains this polygon");
+			}
+
 			edge.AddPolygon(polygon);
 		}
 	}
