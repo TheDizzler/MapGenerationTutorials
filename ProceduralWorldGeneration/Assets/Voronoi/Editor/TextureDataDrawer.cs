227a589b189184836a93b5da02bcfc908bb229b2
TheDizzler <tgillon.music@gmail.com>
2021-02-24 03:57:05 +0900

    nicer heightmap color UI

---
+++ ProceduralWorldGeneration/Assets/Voronoi/Editor/TextureDataDrawer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace AtomosZ.Voronoi.EditorTools
+{
+	[CustomPropertyDrawer(typeof(VoronoiTextureData))]
+	public class TextureDataDrawer : PropertyDrawer
+	{
+		private ReorderableList colorHeightList;
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			// The 6 comes from extra spacing between the fields (2px each)
+			if (colorHeightList == null)
+				return base.GetPropertyHeight(property, label);
+			return (EditorGUIUtility.singleLineHeight + 10) * colorHeightList.count;
+		}
+
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			if (colorHeightList == null)
+			{
+				colorHeightList = BuildColorHeightList(property);
+			}
+
+			colorHeightList.DoList(position);
+			EditorGUI.EndProperty();
+		}
+
+
+		private ReorderableList BuildColorHeightList(SerializedProperty property)
+		{
+			var colorHeightMaps = property.FindPropertyRelative("colorHeightMaps");
+			ReorderableList list = new ReorderableList(property.serializedObject, colorHeightMaps, true, true, true, true);
+
+			list.drawHeaderCallback = (Rect rect) =>
+			{
+				EditorGUI.LabelField(rect, "Color Height Map");
+			};
+
+			list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+			{ // an alternative to this would be to make another custom property drawer to encapsulate these
+				var colorHeightMap = colorHeightMaps.GetArrayElementAtIndex(index);
+				Rect colorRect = rect;
+				colorRect.width *= .5f;
+				Rect baseStartRect = colorRect;
+				baseStartRect.x += colorRect.width;
+				EditorGUIUtility.labelWidth = 50;
+				colorHeightMap.FindPropertyRelative("baseColor").colorValue 
+					= EditorGUI.ColorField(colorRect, new GUIContent("Color"),
+					colorHeightMap.FindPropertyRelative("baseColor").colorValue, true, true, false);
+				colorHeightMap.FindPropertyRelative("baseStartHeight").floatValue
+					= EditorGUI.Slider(baseStartRect, new GUIContent("Start Height"),
+					colorHeightMap.FindPropertyRelative("baseStartHeight").floatValue, 0, 1);
+			};
+
+			return list;
+		}
+	}
+}
--- ProceduralWorldGeneration/Assets/Voronoi/VoronoiTextureData.cs
+++ ProceduralWorldGeneration/Assets/Voronoi/VoronoiTextureData.cs
@@ -1,13 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace AtomosZ.Voronoi
 {
 	[System.Serializable]
 	public class VoronoiTextureData
 	{
-		public Color[] baseColors;
-		[Range(0, 1)]
-		public float[] baseStartHeights;
+		public List<ColorHeightMapData> colorHeightMaps;
 
 		float savedMinHeight;
 		float savedMaxHeight;
@@ -15,17 +14,52 @@ namespace AtomosZ.Voronoi
 
 		public void ApplyToMaterial(Material material)
 		{
-			material.SetInt("layerCount", baseColors.Length);
-			material.SetColorArray("baseColors", baseColors);
-			material.SetFloatArray("baseStartHeights", baseStartHeights);
+			if (colorHeightMaps.Count == 0)
+			{
+				Debug.LogWarning("No color map set");
+				return;
+			}
+
+			material.SetInt("layerCount", colorHeightMaps.Count);
+			material.SetColorArray("baseColors", GetBaseColors());
+			material.SetFloatArray("baseStartHeights", GetBaseStartHeights());
 		}
 
 		public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
 		{
+			if (colorHeightMaps.Count == 0)
+			{
+				return;
+			}
+
 			savedMinHeight = minHeight;
 			savedMaxHeight = maxHeight;
 			material.SetFloat("minHeight", minHeight);
 			material.SetFloat("maxHeight", maxHeight);
 		}
+
+		private List<float> GetBaseStartHeights()
+		{
+			List<float> startHeights = new List<float>();
+			foreach (var chm in colorHeightMaps)
+				startHeights.Add(chm.baseStartHeight);
+			return startHeights;
+		}
+
+		private List<Color> GetBaseColors()
+		{
+			List<Color> colors = new List<Color>();
+			foreach (var chm in colorHeightMaps)
+				colors.Add(chm.baseColor);
+			return colors;
+		}
+	}
+
+	[System.Serializable]
+	public class ColorHeightMapData
+	{
+		public Color baseColor;
+		[Range(0, 1)]
+		public float baseStartHeight;
 	}
 }
